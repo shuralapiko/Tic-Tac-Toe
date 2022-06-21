652ad6bd1ef5eee1ffbb56cef912fbfb46f305e6
DESKTOP-J3N9UNS\serge <sergey.hural@gmail.com>
2022-06-21 08:59:42 +0300

    init commit

---
+++ database/migrations/2022_06_20_213158_create_games_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+
+            $table->decimal('player_x', 9, 0)->default(0);
+            $table->decimal('player_o', 9, 0)->default(0);
+
+            $table->char('start_turn')->default('x');
+            $table->char('victory')->default('');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
